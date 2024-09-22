0cdd4460b0434ae8a08b92b42ad58f5d4e1a7a30
sergiolix <sergiolix8@gmail.com>
2024-09-22 18:47:03 -0500

    Se crean Varias pantallas para su posterior uso

--- app/src/main/java/com/example/prototipoeda/Categoria1.kt
+++ app/src/main/java/com/example/prototipoeda/Categoria1.kt
@@ -2,29 +2,33 @@ package com.example.prototipoeda
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Menu : AppCompatActivity() {
+class Categoria1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
+        setContentView(R.layout.activity_categoria1)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val BackButton = findViewById<ImageButton>(R.id.BotonAtras2)
+        val BotonEjercicio1= findViewById<ImageButton>(R.id.BotonEjercicio1)
 
-        val BotonCtg1 = findViewById<Button>(R.id.BotonCtg1)
+        BackButton.setOnClickListener{
+             val intent = Intent(this,Menu::class.java)
+             startActivity(intent)
+        }
 
-        BotonCtg1.setOnClickListener{
-            val intent = Intent(this,Categoria1::class.java)
+        BotonEjercicio1.setOnClickListener{
+            val intent = Intent (this,Ejercicio1::class.java)
             startActivity(intent)
-
         }
     }
 }
--- app/src/main/java/com/example/prototipoeda/Ejercicio2.kt
+++ app/src/main/java/com/example/prototipoeda/Ejercicio2.kt
@@ -1,30 +1,20 @@
 package com.example.prototipoeda
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Menu : AppCompatActivity() {
+class Ejercicio2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
+        setContentView(R.layout.activity_ejercicio2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val BotonCtg1 = findViewById<Button>(R.id.BotonCtg1)
-
-        BotonCtg1.setOnClickListener{
-            val intent = Intent(this,Categoria1::class.java)
-            startActivity(intent)
-
         }
     }
 }
